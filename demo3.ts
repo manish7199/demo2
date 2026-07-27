442188126d9b00a920cc48b71ee20212e5287169
Manish <manish@MacBook-Air-M2.local>
2026-07-28 00:17:55 +0530

    demo3.ts

---
+++ demo3.ts
@@ -0,0 +1,18 @@
+import { test, expect } from "@playwright/test";
+
+test("Login to OrangeHRM", async ({ page }) => {
+
+    await page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+
+    await page.locator("//input[@placeholder='Username']").fill("Admin");
+await page.waitForTimeout(3000);
+    await page.locator("//input[@placeholder='Password']").fill("admin123");
+await page.waitForTimeout(3000);
+    await page.locator("//button[@type='submit']").click();
+ await page.waitForTimeout(1000);  
+    // Assertion
+    await expect(page).toHaveURL(/dashboard/);
+
+    await expect(page.locator("//h6[text()='Dashboard']")).toBeVisible();
+
+});
